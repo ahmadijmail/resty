7f667eb61190acdaf0804897bce7647b207d898e
ahmadijmail <ahmadijmil2010@gmail.com>
2022-08-21 00:00:49 +0300

    last styling

--- src/app.js
+++ src/app.js
@@ -1,23 +1,14 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React  from "react";
 import "./app.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// Let's talk about using index.js and some other name in the component folder
-// There's pros and cons for each way of doing this ...
 import Header from "./components/header";
 import Footer from "./components/footer";
-// import Form from "./components/form";
-// import Results from "./components/results";
+
 import History from "./components/history/history";
 import Home from "./components/Home/Home";
 
 function App() {
-  // const [data, setState] = useState([]);
-  // const [reqest, setformData] = useState([]);
 
-  // const callApi = async (data, formData) => {
-  //   setState(data);
-  //   setformData(formData);
-  // };
 
   return (
     <React.Fragment>
